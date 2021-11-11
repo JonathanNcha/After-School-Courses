81390d8ae77faa1128d32576f1e265ea93139531
JonathanNcha <93585577+JonathanNcha@users.noreply.github.com>
2021-11-11 12:53:46 +0400

    Add files via upload
    Added the javascript file containing the lessons

---
+++ lessons.js
@@ -0,0 +1,91 @@
+let lessons = [
+  {
+    "id": 10,
+    "title": "Math",
+    "location": "London",
+    "price": 100,
+    "availableSpace": 5,
+    "image": "far fa-calculator-alt fa-6x"
+  },
+  {
+    "id": 11,
+    "title": "English",
+    "location": "Texas",
+    "price": 130,
+    "availableSpace": 5,
+    "image": "fas fa-language fa-6x"
+  },
+  {
+    "id": 12,
+    "title": "Physics",
+    "location": "Dubai",
+    "price": 120,
+    "availableSpace": 5,
+    "image": "fas fa-space-shuttle fa-6x"
+    
+  },
+  {
+    "id": 13,
+    "title": "Chemistry",
+    "location": "Abuja",
+    "price": 130,
+    "availableSpace": 5,
+    "image": "fas fa-atom fa-6x"
+  },
+  {
+    "id": 14,
+    "title": "Biology",
+    "location": "Michigan",
+    "price": 110,
+    "availableSpace": 5,
+    "image": "fas fa-flask fa-6x"
+  },
+  {
+    "id": 15,
+    "title": "Music",
+    "location": "Nairobi",
+    "price": 150,
+    "availableSpace": 5,
+    "image": "fas fa-music fa-6x"
+  },
+  {
+    "id": 16,
+    "title": "Photography",
+    "location": "Tokyo",
+    "price": 160,
+    "availableSpace": 5,
+    "image": "far fa-camera-alt fa-6x"
+  },
+  {
+    "id": 17,
+    "title": "Swimming",
+    "location": "Seoul",
+    "price": 200,
+    "availableSpace": 5,
+    "image": "fas fa-swimmer fa-6x"
+  },
+  {
+    "id": 18,
+    "title": "Soccer",
+    "location": "Mumbai",
+    "price": 170,
+    "availableSpace": 5,
+    "image": "fas fa-futbol fa-6x"
+  },
+  {
+    "id": 19,
+    "title": "Chess",
+    "location": "Moscow",
+    "price": 200,
+    "availableSpace": 5,
+    "image": "fas fa-chess fa-6x"
+  },
+  {
+    "id": 20,
+    "title": "Robotics",
+    "location": "New York",
+    "price": 250,
+    "availableSpace": 5,
+    "image": "fas fa-robot fa-6x"
+  }
+]
